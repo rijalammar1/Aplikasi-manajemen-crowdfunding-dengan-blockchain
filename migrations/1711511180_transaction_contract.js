ee52d32153183ea7e6cffa9489d169df3fe2e3c2
Rijal Ammar Irsyadul Ibad <rijaltn13@gmail.com>
2024-03-27 12:22:22 +0700

    Delete migrations directory

--- migrations/1711511180_transaction_contract.js
+++
@@ -1,5 +0,0 @@
-const TransactionContract = artifacts.require("TransactionContract"); // Importing the contract file assigned to the variable TransactionContract
-
-module.exports = function(deployer) {
-  deployer.deploy(TransactionContract); // Deploying the contract
-};
